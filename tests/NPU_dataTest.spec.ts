b5e06e3e5261ac2f9a9091d5d92f70e295a8b627
Michala <michaladostalova@email.com>
2025-08-05 15:04:51 +0200

    not

--- tests/NPU_dataTest.spec.ts
+++ tests/NPU_dataTest.spec.ts
@@ -2,9 +2,14 @@ import{test, expect} from '@playwright/test';
 const URL = 'https://www.npu.cz/cs'
 
 test.describe('dataTest', () => {
-  test('title', async ({ page }) => {
+  test('DataTest', async ({ page }) => {
     await page.goto(URL);
      const extractedDates: string[] = [];
+     await page.getByRole('button', { name: 'Všechny akce' }).click();
+     expect(page).toHaveURL(/.*akce/);
+     await page.getByRole('link', { name: '7AC46ED7-0EE7-42C3-A2F4-DEAB1D8B167F dalších 97' }).click();
+      await page.waitForLoadState('networkidle');
+
     for (let i = 0; i < 5; i++) {
       const event = page.locator('.events__item').nth(i);
       await expect(event).toBeVisible();
@@ -14,12 +19,19 @@ test.describe('dataTest', () => {
       const textEvent = await dateEvent.textContent();
       console.log(textEvent)
       if(textEvent) {
-        const dataMatch = textEvent.match(/\d{1,2}\.\s*\d{1,2}\.\s*\d{4}/)
-        if(dataMatch) {
-          extractedDates.push(dataMatch[0].trim());
+     const cleaned = textEvent.trim();
+        
+     
+        const singleDayPattern = /^\d{1,2}\.\s*\d{1,2}\.\s*\d{4}$/;
+
+        if (singleDayPattern.test(cleaned)) {
+          extractedDates.push(cleaned);
+          console.log(`Single-day event: ${cleaned}`);
+        } else {
+          console.log(`Multi-day or invalid format skipped: ${cleaned}`);
         }
       }
- }
+     }
  const chronologicalDates = extractedDates.sort((a, b) => {
       const [dayA, monthA, yearA] = a.split('.').map(Number);
       const [dayB, monthB, yearB] = b.split('.').map(Number);
