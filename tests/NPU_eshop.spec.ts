afcd528b3baf5b4083d0370291397e60b43c919c
Michala <michaladostalova@email.com>
2025-10-02 14:57:04 +0200

    test_eshop

---
+++ tests/NPU_eshop.spec.ts
@@ -0,0 +1,20 @@
+//cesta z homepage do detailu produktu
+//vyhledavani podle jmena
+//filtr podle roku
+//filtr podle vydavatele
+//filtr podle dostupnosti
+//filtr podle  typ knihy
+//ražení podle ceny
+//do kose a smazat
+
+
+import { test, expect } from '@playwright/test';
+import { describe } from 'node:test';
+const URL = 'https://eshop.npu.cz/cs/';
+
+test.describe('NPU Eshop Tests', () => {
+
+    test("workflow", async({page}) =>{
+
+    });
+});
